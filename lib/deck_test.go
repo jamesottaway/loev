515c96904088fe7186b3c6999e05dc50d76356c5
James Ottaway <git@james.ottaway.io>
2014-11-25 08:35:36 +1000

    Improve the Deck type, with tests this time

--- lib/deck_test.go
+++ lib/deck_test.go
@@ -29,25 +29,65 @@ func TestDrawReturnsTheRemainingDeck(t *testing.T) {
 	}
 }
 
-func TestShuffleReturnsDeckWithTheSameCardsButInDifferentOrder() (t *testing.T) {
+func TestShuffleReturnsDeckWithTheSameCardsButInDifferentOrder(t *testing.T) {
 	deck := loev.NewDeck()
 	shuffled := deck.Shuffle()
 
+	unshuffledCards := make([]loev.Card, 0)
+	shuffledCards := make([]loev.Card, 0)
+
 	for !deck.Empty() {
+		card, remaining := deck.Draw()
+		deck = remaining
+		unshuffledCards = append(unshuffledCards, card)
 	}
 
 	for !shuffled.Empty() {
+		card, remaining := shuffled.Draw()
+		shuffled = remaining
+		shuffledCards = append(shuffledCards, card)
+	}
+
+	matching := 0
+	for i, card := range shuffledCards {
+		if card == unshuffledCards[i] {
+			matching += 1
+		}
+	}
+
+	if matching >= len(shuffledCards)/2 {
+		t.Errorf("Expected less that %d cards to match after shuffling, but was %d", len(shuffledCards)/2, matching)
 	}
 }
 
-func TestShuffleIsNonDeterministicAndReturnsDifferentOrderEachTime() (t *testing.T) {
+func TestShuffleIsNonDeterministicAndReturnsDifferentOrderEachTime(t *testing.T) {
 	deck := loev.NewDeck()
 	shuffleOne := deck.Shuffle()
 	shuffleTwo := deck.Shuffle()
 
+	firstShuffle := make([]loev.Card, 0)
+	secondShuffle := make([]loev.Card, 0)
+
 	for !shuffleOne.Empty() {
+		card, remaining := shuffleOne.Draw()
+		shuffleOne = remaining
+		firstShuffle = append(firstShuffle, card)
 	}
 
 	for !shuffleTwo.Empty() {
+		card, remaining := shuffleTwo.Draw()
+		shuffleTwo = remaining
+		secondShuffle = append(secondShuffle, card)
+	}
+
+	matching := 0
+	for i, card := range firstShuffle {
+		if card == secondShuffle[i] {
+			matching += 1
+		}
+	}
+
+	if matching >= len(secondShuffle)/2 {
+		t.Errorf("Expected less that %d cards to match after shuffling, but was %d", len(secondShuffle)/2, matching)
 	}
 }
